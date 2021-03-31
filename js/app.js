6877139e47711547c15f34eb6f50fe838a506251
Sanabel8 <sanabeljamal97@gmail.com>
2021-03-31 13:48:36 +0300

    function for qustion 1

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ alert('Ready? Lets go ' + userName);
 
 let userScore = 0
 
+
+
+function question1 (){
+
 // first question
 let myNationality = prompt('Do you think I am Jordanian?');
 myNationality = myNationality.toLowerCase();
@@ -21,9 +25,13 @@ if (myNationality === 'yes' || myNationality === 'y') {
 } else {
     alert('you should answer with either yes/y or no/n next time');
 }
-
+}
+question1();
 
 // second question
+
+
+//function secondQuestion(){
 let myBorn = prompt('Ok now do you think I was borned in Jordan?');
 myBorn = myBorn.toLowerCase();
 // console.log(myBorn);
@@ -37,7 +45,8 @@ if (myBorn === 'yes' || myBorn === 'y') {
     alert('you should answer with either yes/y or no/n next time');
 }
 
-
+//}
+secondQuestion();
 // third question
 let myEducation = prompt('Do you think I have a bachelor degree in software development?');
 myEducation = myEducation.toLowerCase();
